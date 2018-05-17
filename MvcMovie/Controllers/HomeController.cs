14a22679e4b8342b5513e15af3e9c63227ee1b3d
Rael <184766@lanl.gov>
2018-05-17 14:29:36 -0600

    Add project files.

---
+++ MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class Home : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+        //GET: /Home/About/
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        //GET: /Home/Contact
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
